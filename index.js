5da45b11909ea9d92896ec06a555e667bdad38d5
m1yh3m <m1yh3m@protonmail.com>
2019-12-22 13:49:30 +0530

    Working tool
    Requires testing

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 #!/usr/bin/env node
 
+const program = require('commander')
 const dns = require('dns')
+const red = require('@f0c1s/color-red')
+const yellow = require('@f0c1s/color-yellow')
 
-console.log('hello world from dns_lookup')
+function lookup (domain) {
+  dns.lookup(domain, (err, address, family) => {
+    if (err) {
+      console.error(`ERROR: ${err.message}`)
+      return err
+    }
+    console.log(`${yellow(domain)} at ${red(address)} via IPv${red(family)}`)
+  })
+}
 
+program.arguments('<domain>')
+  .action(lookup)
+  .parse(process.argv)
